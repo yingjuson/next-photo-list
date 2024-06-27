4f59c3e80f6e25ebf9e27c136b0f0f25a79ce021
yngjsn <jusonjamesjohn@gmail.com>
2024-06-27 21:32:51 +0800

    chore: remove unnecessary comments

--- store/useSearchStore.ts
+++ store/useSearchStore.ts
@@ -1,15 +1,5 @@
 import { create } from "zustand";
 
-// interface BearState {
-//   bears: number;
-//   increase: (by: number) => void;
-// }
-
-// const useSearchStore = create<BearState>()((set) => ({
-//   bears: 0,
-//   increase: (by) => set((state) => ({ bears: state.bears + by })),
-// }));
-
 interface ISearchStore {
   searchQuery: string;
   setSearchQuery: (queryString: string) => void;
